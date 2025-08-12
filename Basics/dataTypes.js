259232033f930960d4abc328bd068421e5900014
Dipesh <62205185+dips98@users.noreply.github.com>
2025-08-12 14:03:06 +0000

    Data types

---
+++ Basics/dataTypes.js
@@ -0,0 +1,11 @@
+/*
+8 Data Types in JS
+1. undefined - undefined
+2. null - null
+3. Boolean - boolean
+4. Number - number
+5. String - string
+6. Object - object
+7. Bigint - bigint
+8. Symbol
+*/
